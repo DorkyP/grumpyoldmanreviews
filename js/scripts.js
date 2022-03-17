c948afe9bada83e2e7d825a381f9048ff91ce820
DorkyP <64234510+DorkyP@users.noreply.github.com>
2022-03-17 11:56:19 +1100

    Moved navbar into JS

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,27 @@ window.addEventListener('DOMContentLoaded', () => {
 		scrollPos = currentTop;
 	});
 });
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+		<!-- Navigation-->
+		<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+			<div class="container px-4 px-lg-5">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				Menu
+					<i class="fas fa-bars"></i>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ms-auto py-4 py-lg-0">
+						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/index.html">Home</a></li>
+						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/about.html">About</a></li>
+						<!-- <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li> -->
+					</ul>
+				</div>
+			</div>
+		</nav>
+`;
+
+document.body.prepend(template.content);
+// document.body.insertBefore(template.content,document.body.header);
