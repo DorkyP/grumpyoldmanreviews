287c7354a3306905a870ec32e3f6f64ce002e32f
DorkyP <64234510+DorkyP@users.noreply.github.com>
2022-03-17 11:08:36 +1100

    Finished getting review summaries to insert from XML into index page

--- js/index.js
+++ js/index.js
@@ -16,7 +16,18 @@ function addLatestMovieReviews(xml) {
 	for (i = x.length-1; i >= 0; i--) {
 		var title = x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
 		var tagline = x[i].getElementsByTagName("tagline")[0].childNodes[0].nodeValue;
-		var published = x[i].getElementsByTagName("published")[0].childNodes[0].nodeValue;
+		var publishedDate = new Date(x[i].getElementsByTagName("published")[0].childNodes[0].nodeValue);
+
+		// Here's some clunky code to make the date look better. Why isn't this something built in to JS or jQuery or something?!? ¯\_(ツ)_/¯
+		const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+		let month = months[publishedDate.getMonth()];
+		let day = publishedDate.getDate();
+		let suffix = (day >= 4 &&  day <= 20) || (day >= 24 && day <= 30)
+			? "th"
+			: ["st", "nd", "rd"][day % 10 - 1];
+		let year = publishedDate.getFullYear()
+		prettyPublishedDate = month + " " + day + suffix + ", " + year;
+
 		var filename = title.replaceAll(' ', '-')
 		table += `<!-- Post preview-->
 					<div class="post-preview">
@@ -25,7 +36,7 @@ function addLatestMovieReviews(xml) {
 							<h3 class="post-subtitle">` + tagline + `</h3>
 						</a>
 						<p class="post-meta">
-							Posted on ` + published + `
+							Posted on ` + prettyPublishedDate + `
 						</p>
 					</div>
 					<!-- Divider-->
