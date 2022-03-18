bb64cb321545b268982f56c696df60438a50f377
DorkyP <64234510+DorkyP@users.noreply.github.com>
2022-03-18 16:26:45 +1100

    Added 'Contact' to navbar

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
 						<ul class="navbar-nav ms-auto py-4 py-lg-0">
 							<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</a></li>
 							<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/public/about">About</a></li>
-							<!-- <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/public/contact">Contact</a></li> -->
+							<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/public/contact">Contact</a></li>
 						</ul>
 					</div>
 				</div>
